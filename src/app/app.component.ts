d887f7abdd086d1d001f5b7601f8c0a7d23acd9e
toprasit2 <toprasit.gk@gmail.com>
2018-07-22 13:58:28 +0700

    add home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class MyApp {
   rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    //ทุกอย่างโหลดแล้วให้ทำอะไรต่อ
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
